c555f3d77febee83c6976cd36b072d597e2e7ab4
David HUANG <chuangag@connect.ust.hk>
2016-10-04 12:59:53 +0800

    New GameBoard files

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,7 +1,15 @@
+#include <QApplication>
 #include <QtWidgets>
 #include <stdlib.h>
 #include "MainWindow.h"
-
+ #include <q3mainwindow.h>
+#include <qapplication.h>
+#include <qpixmap.h>
+#include <qtimer.h>
+//Added by qt3to4:
+#include <QPaintEvent>
+#include <QLabel>
+#include <QtGui>
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
