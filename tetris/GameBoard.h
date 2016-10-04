4e0239935968dee47c7dae7d7bc60814e149e977
gaotongxiao <gaotongxiao@gmail.com>
2016-10-04 14:39:33 +0800

    hi

--- tetris/GameBoard.h
+++ tetris/GameBoard.h
@@ -23,6 +23,7 @@ public:
   void tryrotateClock();
   void tryrotateCounterclock();
   void setNextBlock();
+  void getNextBlock();
   
 public slots:
   void start();
@@ -32,11 +33,11 @@ signals:
   void changeLevel(int level);
   
 private:
-  int landedBoard[9][19];//10*20 board
-  int tempBoard[9][19];//landed blocks + moving blocks
+  int landedBoard[10][20];//10*20 board
+  int tempBoard[10][20];//landed blocks + moving blocks
   int currentX;//center's x coordinate
   int currentY;//center's y coordinate
-  int currentBlock[4][4];//any blocks can be put in a 4*4 grid
+  //int currentBlock[4][4];//any blocks can be put in a 4*4 grid
   int nextBlock[4][4];
 }
 
